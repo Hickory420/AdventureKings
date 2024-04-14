88e49f3ed7fc0f5073fbc43f0efe5ad61e65d628
Chris Riggs <chris@techriggs.net>
2024-04-15 00:06:53 +1000

    Save json output to file

--- kings.py
+++ kings.py
@@ -18,6 +18,7 @@ The main capabilities are:
 
 import argparse
 from lib import adventurekings
+from typing import Any
 
 
 argparse.ArgumentParser(description='Scrape 4WD Supacentre website for URLs')
@@ -42,8 +43,20 @@ parser.add_argument('--deals', '-d',
                     help='Show Daily Deals',
                     action='store_true')
 
+parser.add_argument('--write', '-w',
+                    help='Save json output to file',
+                    type=str)
+
 args = parser.parse_args()
 
+
+def write_to_file(data: list[dict[str, Any]], file: str):
+    """
+    Writes data to a file.
+    """
+    print(f'writing to file {args.write}')
+    with open(file, mode='w', encoding='utf-8') as f:
+        f.write(f'{data}')
 
 def main():
     """
@@ -64,20 +77,28 @@ def main():
     if args.batteries:
         battery_data = kings.battery_prices()
         kings.print_prices(battery_data, batteries=True)
+        if args.write:
+            write_to_file(battery_data, args.write)
         raise SystemExit(0)
 
     if args.search:
         search_data = kings.search(args.search)
         kings.print_prices(search_data)
+        if args.write:
+            write_to_file(search_data, args.write)
         raise SystemExit(0)
 
     if args.deals:
         deals_data = kings.daily_deals()
         kings.print_prices(deals_data)
+        if args.write:
+            write_to_file(deals_data, args.write)
         raise SystemExit(0)
 
     csv_data = kings.prices_from_csv(csv_file)
     kings.print_prices(csv_data)
+    if args.write:
+        write_to_file(csv_data, args.write)
 
 
 if __name__ == "__main__":
